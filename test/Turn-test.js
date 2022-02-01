d1b29dc165fdf1685b29e7abec796adae3d101c2
Sam Mistele <smmstl0@gmail.com>
2022-02-01 15:49:45 -0700

    Add test requirements and describe

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,1 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
 
+const Turns = require('../src/Turns');
+
+describe('Turns', function() {
+
+  it('should be a function', function() {
+    const turns = new Turns();
+    expect(Turns).to.be.a('function');
+  });
+
+  it('should be instantiated with two arguments', function() {
+    const turns = new Turns(guess, )
+  })
+});
